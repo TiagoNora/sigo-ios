b9a99fa260d1cbd096bcd170549f5a5f8adea906
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2026-01-22 16:52:06 +0000

    Delete onecare directory

--- onecare/sigo/android/app/src/main/kotlin/com/alticelabs/sigo/onecare/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.alticelabs.sigo.onecare
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- onecare/sigo/lib/config/app_features.dart
+++
@@ -1,7 +0,0 @@
-/// Centralized feature toggles for the app.
-class AppFeatures {
-  AppFeatures._();
-
-  /// Gate notifications UI entry points.
-  static const bool enableNotifications = true;
-}
